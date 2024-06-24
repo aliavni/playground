dc49510405b7054a8aaf0b22832767db4bc82f06
Ali Cirik <aliavni@gmail.com>
2024-06-23 20:50:35 -0400

    dist

---
+++ dist/assets/AboutView-D6nLH4XB.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,a as s}from"./index-Crt2Eu9B.js";const c={},a={class:"about"},_=s("h1",null,"This is the about page",-1),n=[_];function r(i,d){return o(),t("div",a,n)}const l=e(c,[["render",r]]);export{l as default};
